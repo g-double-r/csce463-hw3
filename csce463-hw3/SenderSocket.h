d57ccf875520dad8f3517fc7b11df8fd3b93848c
g-double-r <gramirez-rodarte@tamu.edu>
2025-10-20 18:17:28 -0500

    added some preliminaries

---
+++ csce463-hw3/SenderSocket.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#ifdef __APPLE__
+	#include <errno.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <sys/select.h>
+	#include <sys/time.h>
+	#include <sys/types.h>
+	#include <unistd.h>
+	#define WSAGetLastError() (errno)
+	#define WSACleanup() ((void)0)
+	typedef int SOCKET;
+	typedef uint32_t DWORD;
+	#define INVALID_SOCKET (-1)
+	#define SOCKET_ERROR (-1)
+#else
+	#include <WinSock2.h>
+	#include <WS2tcpip.h>
+	#pragma comment(lib, "Ws2_32.lib")
+#endif
+
+// CONSTANTS
+#define MAGIC_PORT 22345 // receiver listens on this port
+#define MAX_PKT_SIZE (1500-28) // maximum UDP packet size accepted by receiver
+
+// possible status codes from ss.Open, ss.Send, ss.Close
+#define STATUS_OK 0 // no error
+#define ALREADY_CONNECTED 1 // second call to ss.Open() without closing connection
+#define NOT_CONNECTED 2 // call to ss.Send()/Close() without ss.Open()
+#define INVALID_NAME 3 // ss.Open() with targetHost that has no DNS entry
+#define FAILED_SEND 4 // sendto() failed in kernel
+#define TIMEOUT 5 // timeout after all retx attempts are exhausted
+#define FAILED_RECV 6 // recvfrom() failed in kernel
+
+class SenderSocket
+{
+};
+
