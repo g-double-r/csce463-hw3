24d74b961469558954a85fd6c545cfd052039481
g-double-r <gramirez-rodarte@tamu.edu>
2025-11-04 13:15:48 -0600

    fixed for windows

--- csce463-hw3/pch.h
+++ csce463-hw3/pch.h
@@ -8,30 +8,12 @@
 #define PCH_H
 
 // add headers that you want to pre-compile here
-#ifdef __APPLE__
-	#include <errno.h>
-	#include <sys/socket.h>
-	#include <netinet/in.h>
-	#include <arpa/inet.h>
-	#include <sys/select.h>
-	#include <sys/time.h>
-	#include <sys/types.h>
-	#include <unistd.h>
-    #include <netdb.h>
-	#define WSAGetLastError() (errno)
-	#define WSACleanup() ((void)0)
-	typedef int SOCKET;
-	typedef uint32_t DWORD;
-	#define INVALID_SOCKET (-1)
-	#define SOCKET_ERROR (-1)
-#else
 	#define _WINSOCK_DEPRECATED_NO_WARNINGS
 	#define NOMINMAX
 	#include <WinSock2.h>
 	#include <WS2tcpip.h>
 	#include <windows.h>
 	#pragma comment(lib, "Ws2_32.lib")
-#endif
 
 #include "SenderSocket.h"
 #include "PacketHeaders.h"
